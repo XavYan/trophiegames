ee1197aef70eb2ca58df96be0ebe892c0f7d7f24
XavYan <xavieryanquin@gmail.com>
2020-02-25 01:16:02 +0100

    Script de automatización creado. A partir de ahora se pueden actualizar todos los footers modificando la plantilla y ejecutando este script

---
+++ testing/update_header_footer.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+
+import os
+
+# Obtenemos el header y el footer de la plantilla
+plantilla = open("plantilla.html", "r")
+plantContent = plantilla.read()
+
+plantBeginHeaderIndex = plantContent.find("<header>")
+plantEndHeaderIndex = plantContent.find("</header>", plantBeginHeaderIndex) + len("</header>") - 1
+
+plantBeginFooterIndex = plantContent.find('<footer class="footer">')
+plantEndFooterIndex = plantContent.find("</footer>", plantBeginFooterIndex) + len("</footer>") - 1
+
+plantHeader = plantContent[plantBeginHeaderIndex:plantEndHeaderIndex+1]
+plantFooter = plantContent[plantBeginFooterIndex:plantEndFooterIndex+1]
+
+# Obtenemos todos los ficheros HTML del proyecto
+filenames = []
+
+for root, dirs, files in os.walk('.'):
+    for filename in files:
+        if os.path.splitext(filename)[1] == ".html" and filename != "plantilla.html":
+            filenames.append(filename)
+
+for filename in filenames:
+    fileData = open(filename, "r")
+    data = fileData.read()
+
+    # Header
+    beginHeaderIndex = data.find("<header>")
+    endHeaderIndex = data.find("</header>", beginHeaderIndex) + len("</header>") - 1
+
+    # Footer
+    beginFooterIndex = data.find('<footer class="footer">')
+    endFooterIndex = data.find("</footer>", beginFooterIndex) + len("</footer>") - 1
+
+    finalFile = data[:beginHeaderIndex-1] + plantHeader + data[endHeaderIndex+1:beginFooterIndex] + plantFooter + data[endFooterIndex+1:]
+
+    # Sustituimos el header y el footer antiguo por el nuevo
+    fileData.close()
+    fileData = open(filename, "w")
+    fileData.write(finalFile)
